4ab3bb17221cc84200ba9ba69b634e23b6995cae
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2022-10-04 22:49:11 +0530

    Add files via upload

---
+++ Python/Password_generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def get_random_string(length):
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+    print(result_str)
+
+get_random_string(12)
